e4978feb18fbe755cfbd0328406a3754c749134c
KalaiBK <kalaiboopathy18@gmail.com>
2023-10-22 21:02:32 +0530

    first commit

---
+++ jsoncomp.js
@@ -0,0 +1,23 @@
+let obj1 = { name: "Person 1", age: 5 }
+let obj2 = { age: 5, name: "Person 1" }
+var flag = true;
+if (Object.keys(obj1).length == Object.keys(obj2).length) {
+    for (key in obj1) {
+        if (obj1[key] == obj2[key]) {
+            continue;
+        } else {
+            flag = false;
+            break;
+        }
+    }
+
+} else {
+
+    flag = false;
+}
+if(flag == true) {
+    console.log("Is Equal");
+} else {
+    console.log("Not Equal")
+}
+
